a721838d47f6b3300d8839332bfe04646987d798
prasannadhaygude1 <Dhaygude1507@gmail.com>
2024-07-20 17:15:14 +0530

    Added Routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from './views/Home/Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Nursery Server</h1>);
+
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <Home />
+    },
+    {
+        path : "*",
+        element : <h1>404 Not Found</h1>
+    }
+])
+
+root.render(<RouterProvider router={router} />);
