0278e8bcb3b34562979d8ca7b726bd1c60642ec0
prasannadhaygude1 <Dhaygude1507@gmail.com>
2024-07-21 13:33:39 +0530

    Added Form to add plant

--- src/views/AddPlant/AddPlant.js
+++ src/views/AddPlant/AddPlant.js
@@ -1,10 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./AddPlant.css"
+import toast, { Toaster } from 'react-hot-toast'
+import axios from 'axios'
 
 function AddPlant() {
-  return (
-    <div>AddPlant</div>
-  )
+
+    const [name, setName] = useState("")
+    const [category, setCategory] = useState("")
+    const [price, setPrice] = useState("")
+    const [image, setImage] = useState("")
+    const [description, setDescription] = useState("")
+
+    const addPlant = async ()=>{
+        toast.loading("Adding Plant...")
+
+        if(!name || !category || !price || !image || !description){
+            toast.error("Please Enter All Details")
+
+            return
+        }
+
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}/plant`,{
+            name: name,
+            price: price,
+            category: category,
+            image: image,
+            description: description
+        })
+
+        toast.dismiss()
+        toast.success(response.data.message)
+
+        setName("")
+        setCategory("")
+        setPrice("")
+        setDescription("")
+        setImage("")
+    }
+
+    return (
+        <div>
+            <h1>Add Plant</h1>
+
+            <form>
+                <input
+                    type='text'
+                    placeholder='Enter Plant Name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className='plant-input'
+                />
+
+                <input
+                    type='number'
+                    placeholder='Enter Plant Price'
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    className='plant-input'
+                />
+
+                <input
+                    type='text'
+                    placeholder='Enter Plant Category'
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    className='plant-input'
+                />
+
+                <input
+                    type='text'
+                    placeholder='Enter Plant image Url'
+                    value={image}
+                    onChange={(e) => setImage(e.target.value)}
+                    className='plant-input'
+                />
+
+                <input
+                    type='text'
+                    placeholder='Enter Plant Description'
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className='plant-input'
+                />
+
+                <button type='button' onClick={addPlant}>Add Plant</button>
+            </form>
+            <Toaster />
+        </div>
+    )
 }
 
 export default AddPlant
